93c3e3987f08ffa1e192681071aaf35113735159
paujim <paujim@hotmail.com>
2020-09-27 17:13:50 +1000

    add cognito user pool

--- apigateway_appsync/apigateway_appsync_stack.py
+++ apigateway_appsync/apigateway_appsync_stack.py
@@ -3,6 +3,8 @@ from aws_cdk import (
     core,
     aws_appsync as appsync,
     aws_dynamodb as dynamodb,
+    aws_cognito as cognito,
+    aws_iam as iam,
 )
 
 
@@ -10,6 +12,29 @@ class ApigatewayAppsyncStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        pool = cognito.UserPool(
+            scope=self,
+            id="user-pool",
+            mfa=cognito.Mfa.OPTIONAL,
+            mfa_second_factor=cognito.MfaSecondFactor(otp=True, sms=True),
+            password_policy=cognito.PasswordPolicy(
+                min_length=12,
+                require_lowercase=True,
+                require_uppercase=False,
+                require_digits=False,
+                require_symbols=False,
+            )
+        )
+
+        client = pool.add_client(
+            id="customer-app-client",
+            auth_flows=cognito.AuthFlow(
+                user_password=True,
+                refresh_token=True,
+                user_srp=True,
+            ),
+        )
 
         api = appsync.GraphqlApi(
             scope=self,
@@ -19,7 +44,10 @@ class ApigatewayAppsyncStack(core.Stack):
                 file_path=os.path.join("graphQL", "schema.graphql")),
             authorization_config=appsync.AuthorizationConfig(
                 default_authorization=appsync.AuthorizationMode(
-                    authorization_type=appsync.AuthorizationType.IAM
+                    authorization_type=appsync.AuthorizationType.USER_POOL,
+                    user_pool_config=appsync.UserPoolConfig(
+                        user_pool=pool,
+                    )
                 )
             ),
             xray_enabled=True
@@ -28,6 +56,7 @@ class ApigatewayAppsyncStack(core.Stack):
         todo_table = dynamodb.Table(
             scope=self,
             id="todo-table",
+            removal_policy=core.RemovalPolicy.DESTROY,
             partition_key=dynamodb.Attribute(
                 name="id",
                 type=dynamodb.AttributeType.STRING
